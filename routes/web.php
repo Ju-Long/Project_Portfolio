640ef628fdda0bae6c2f9c847e1d0cdf68f2f7ac
Ju Long <19013345@myrp.edu.sg>
2021-05-08 23:19:34 +0800

    update on rotues

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Mail\Contact;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\DatabaseController;
 use App\Http\Controllers\BusStop;
 use App\Http\Controllers\QuoteApiController;
 
-Route::get('/test', function () {
-    return round(1.5);
+Route::get('/test', function (Request $req) {
+    date_default_timezone_set("Singapore");
+    $curr = time();
+    return date('Y-m-d', $curr);
 });
 
-Route::get('info', function() { phpinfo();});
+// Route::get('info', function() { phpinfo();});
 
 Route::get('/add_user_has_exercise_data', [DatabaseController::class, 'add_user_has_exercise_data']);
 Route::get('/user', [DatabaseController::class, 'user']);
@@ -23,12 +26,12 @@ Route::get('/user_has_exercise', [DatabaseController::class, 'user_has_exercise'
 Route::get('/user_has_exercise_data', [DatabaseController::class, 'user_has_exercise_data']);
 Route::get('/new_set_done', [DatabaseController::class, 'new_set_done']);
 
-Route::get('/api/get_nearest_bus_stop_code', [BusStop::class, 'get_nearest_busstop']);
-Route::get('/api/get_bus_arrival_timing', [BusStop::class, 'get_bus_stop_timing']);
+Route::get('/api/get_nearest_bus_stop', [BusStop::class, 'get_nearest_bus_stop']);
+Route::get('/api/get_bus_arrival_timing', [BusStop::class, 'get_bus_arrival_timing']);
 Route::get('/api/get_bus_route', [BusStop::class, 'get_bus_route']);
 Route::get('/api/get_bus_data', [BusStop::class, 'search_bus']);
 Route::get('/api/get_bus_stop_data', [BusStop::class, 'get_bus_stop']);
-Route::get('/api/quote-api', [QuoteApiController::class, 'getQuote']);
+Route::get('/api/get_quote', [QuoteApiController::class, 'get_quote']);
 
 Route::get('/api/dashboard', function () {
     return view('api/index');
