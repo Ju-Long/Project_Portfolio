ea9cd18403ed02fe3c7808ecb3fa49586544ac78
Ju Long <19013345@myrp.edu.sg>
2021-05-13 13:02:49 +0800

    update on how database being stored

--- app/Http/Controllers/BusStop.php
+++ app/Http/Controllers/BusStop.php
@@ -23,12 +23,12 @@ class BusStop extends Controller
         $currMonth = date('m', time());
         $currDay = date('Y-m-d', time());
 
-        if (!($lat && $long && $amountReturned>1)) {
+        if (!($lat && $long && $amountReturned > 1)) {
             return [['output' => "Invalid params"]];}
 
         $url = "http://babasama.me/get_nearest_bus_stop/$lat/$long/$amountReturned";
 
-        if (!($user_acc_key || $username)) {
+        if (!($user_acc_key && $username)) {
             $datas = DB::table('api_datacenter.IP_Address_Calls')->select('times_a_day')->where([['IP_address', "$client_ip"], ['day', "$currDay"]])->get();
             foreach($datas as $i) {
                 if ($i->times_a_day > 30) {
@@ -37,8 +37,8 @@ class BusStop extends Controller
 
             DB::table('api_datacenter.IP_Address_Calls')
             ->updateOrInsert(
-                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key],
-                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key, 'day' => "$currDay"],
+                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
             $result = Http::withHeaders([
                 'api_key' => $this->account_key
@@ -52,18 +52,17 @@ class BusStop extends Controller
             return json_decode($result);
         } else {
             $API_Key = DB::table('api_datacenter.User')->select('user_api_key', 'user_role')->where([['user_api_key', $user_acc_key], ['username', "$username"]])->get();
-
             foreach ($API_Key as $i) {
                 if ($i->user_role == "client") {
                     $datas = DB::table('api_datacenter.IP_Address_Calls')->select('times_a_day')->where([['IP_address', "$client_ip"], ['day', $currDay], ['user_api_key', $user_acc_key]])->get();
                     foreach($datas as $i) {
                         if ($i->times_a_day > 300) {
                             return [['output' => 'You use this IP address to call the data too many times for today, please come again another day']];}}}
-
+                            
                 DB::table('api_datacenter.IP_Address_Calls')
                 ->updateOrInsert(
-                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key],
-                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key, 'day' => "$currDay"],
+                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
                 $datamall_key = DB::table('api_datacenter.User_API_Keys')->select('datamall')->where('user_api_key', $i->user_api_key)->get();
                 foreach($datamall_key as $i) {
@@ -97,16 +96,15 @@ class BusStop extends Controller
         $url = "http://babasama.me/get_bus_arrival/$busstopcode";
 
         if (!($user_acc_key || $username)) {
-            $datas = DB::table('api_datacenter.IP_Address_Calls')->select('times_a_day')->where([['IP_address', "$client_ip"], ['day', "$currDay"]])->get();
+            $datas = DB::table('api_datacenter.IP_Address_Calls')->where([['IP_address', "$client_ip"], ['day', "$currDay"]])->get();
             foreach($datas as $i) {
                 if ($i->times_a_day > 30) {
                     return [['output' => 'You use this IP address to call the data too many times anonymously, please register an account if you would like to call more times']];
                 }}
-
             DB::table('api_datacenter.IP_Address_Calls')
             ->updateOrInsert(
-                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key],
-                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key, 'day' => "$currDay"],
+                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
             $result = Http::withHeaders([
                 'api_key' => $this->account_key
@@ -130,8 +128,8 @@ class BusStop extends Controller
 
                 DB::table('api_datacenter.IP_Address_Calls')
                 ->updateOrInsert(
-                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key],
-                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key, 'day' => "$currDay"],
+                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
                 $datamall_key = DB::table('api_datacenter.User_API_Keys')->select('datamall')->where('user_api_key', $i->user_api_key)->get();
                 foreach($datamall_key as $i) {
@@ -172,8 +170,8 @@ class BusStop extends Controller
 
             DB::table('api_datacenter.IP_Address_Calls')
             ->updateOrInsert(
-                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key],
-                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key, 'day' => "$currDay"],
+                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
             $result = Http::withHeaders([
                 'api_key' => $this->account_key
@@ -197,8 +195,8 @@ class BusStop extends Controller
 
                 DB::table('api_datacenter.IP_Address_Calls')
                 ->updateOrInsert(
-                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key],
-                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key, 'day' => "$currDay"],
+                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
                 $datamall_key = DB::table('api_datacenter.User_API_Keys')->select('datamall')->where('user_api_key', $i->user_api_key)->get();
                 foreach($datamall_key as $i) {
@@ -239,8 +237,8 @@ class BusStop extends Controller
 
             DB::table('api_datacenter.IP_Address_Calls')
             ->updateOrInsert(
-                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key],
-                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key, 'day' => "$currDay"],
+                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
             $result = Http::withHeaders([
                 'api_key' => $this->account_key
@@ -264,8 +262,8 @@ class BusStop extends Controller
 
                 DB::table('api_datacenter.IP_Address_Calls')
                 ->updateOrInsert(
-                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key],
-                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key, 'day' => "$currDay"],
+                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
                 $datamall_key = DB::table('api_datacenter.User_API_Keys')->select('datamall')->where('user_api_key', $i->user_api_key)->get();
                 foreach($datamall_key as $i) {
@@ -307,8 +305,8 @@ class BusStop extends Controller
 
             DB::table('api_datacenter.IP_Address_Calls')
             ->updateOrInsert(
-                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key],
-                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                ['IP_address' => "$client_ip", 'month' => $currMonth, 'user_api_key' => $user_acc_key, 'day' => "$currDay"],
+                ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
             $result = Http::withHeaders([
                 'api_key' => $this->account_key
@@ -332,8 +330,8 @@ class BusStop extends Controller
 
                 DB::table('api_datacenter.IP_Address_Calls')
                 ->updateOrInsert(
-                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key],
-                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1'), 'day' => "$currDay"]);
+                    ['IP_address' => "$client_ip", 'month'=> $currMonth, 'user_api_key' => $i->user_api_key, 'day' => "$currDay"],
+                    ['times_a_month' => DB::raw('times_a_month + 1'), 'times_a_day' => DB::raw('times_a_day + 1')]);
 
                 $datamall_key = DB::table('api_datacenter.User_API_Keys')->select('datamall')->where('user_api_key', $i->user_api_key)->get();
                 foreach($datamall_key as $i) {
