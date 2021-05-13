c066981c2254efa04ecd3514cabaf871027c02ef
Ju Long <19013345@myrp.edu.sg>
2021-05-13 22:09:03 +0800

    test to see if it gets post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    function testRequest(Request $req) {
-        return $req['id'] . " plus " . $req['name'];
+
+    function test(Request $req) {
+        $data = $req->input('data', 'no data');
+
+        return $data;
+    }
+
+    function login(Request $req) {
+        $username = $req->input('username');
+        $password = $req->input('password');
+
+        if (!($username || $password)) {
+            return [['output' => "Invalid params"]];}
+        
+        return DB::table('api_datacenter.User')->where([['username', "$username"], ['user_password', "$password"]])->get();
+    }
+
+    function signup(Request $req) {
+        $username = $req->username;
+        $password = $req->password;
+
     }
 }
