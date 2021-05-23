64d0528ce8074f32f336cb58ea70d5a56c6084bb
Ju Long <19013345@myrp.edu.sg>
2021-05-23 20:54:47 +0800

    commit

--- config/value.php
+++ config/value.php
@@ -1,4 +1,5 @@
 <?php
 return [
-    'private_api_key' => env('API_DATACENTER_KEY', '')
+    'private_api_key' => env('API_DATACENTER_KEY', ''),
+    'private_password' => env('API_DELETE_PASSWORD', '')
 ];
