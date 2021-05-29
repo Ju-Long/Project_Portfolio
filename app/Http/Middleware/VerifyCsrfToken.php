47dce4619bfb2450e95253a1bf11fd964517ba45
Ju Long <19013345@myrp.edu.sg>
2021-05-29 16:02:48 +0800

    commit

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,13 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'stripe/*',
-        'http://localhost:8000/email',
         'https://babasama.com/email',
-        'http://localhost:8000/api/signup',
-        'https://babasama.com/api/signup',
-        'http://localhost:8000/api/update_password',
-        'https://babasama.com/api/update_password',
-        'http://localhost:8000/api/generate_code',
-        'https://babasama.com/api/generate_code'
+        'https://babasama.com/api/*'
     ];
 }
